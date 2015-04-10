a8dae1a0a2159c5f2721af1f26c7417a870fdf3d
Daniel Speyer <dspeyer@gmail.com>
2015-04-10 01:48:59 -0400

    use dwarf for call stacks

--- profile.sh
+++ profile.sh
@@ -2,7 +2,7 @@
 
 FN="perf`date|tr -c 0-9 _`.scr"
 
-sudo perf record -a -g -F 999 -e 'sched:sched_wakeup,sched:sched_switch,sched:sched_process_exec,cycles,block:block_bio_queue,sched:sched_process_fork' "$@"
+sudo perf record -a -g --call-graph dwarf -F 999 -e 'sched:sched_wakeup,sched:sched_switch,sched:sched_process_exec,cycles,block:block_bio_queue,sched:sched_process_fork' "$@"
 
 sudo perf script -f trace:tid,comm,time,event,trace,sym,ip,dso > $FN
 
