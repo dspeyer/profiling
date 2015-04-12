bb9e25a819d5a7012170ad552eb33f29f25a44e3
Daniel Speyer <dspeyer@gmail.com>
2015-04-12 02:46:38 -0400

    remove spurious -g and handle invoking from a different directory

--- profile.sh
+++ profile.sh
@@ -2,11 +2,11 @@
 
 FN="perf`date|tr -c 0-9 _`.scr"
 
-sudo perf record -a -g --call-graph dwarf -F 999 -e 'sched:sched_wakeup,sched:sched_switch,sched:sched_process_exec,cycles,block:block_bio_queue,sched:sched_process_fork' "$@"
+sudo perf record -a --call-graph dwarf -F 999 -e 'sched:sched_wakeup,sched:sched_switch,sched:sched_process_exec,cycles,block:block_bio_queue,sched:sched_process_fork' "$@"
 
 sudo perf script -f trace:tid,comm,time,event,trace,sym,ip,dso > $FN
 
 echo "Saved to $FN"
 
-./main.py $FN
+`echo $0 | sed "s/profile.sh/main.py/"` $FN
 
